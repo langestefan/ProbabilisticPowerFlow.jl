2399794ca66ab6bc1445d82041021314bc97ee41
Stefan de Lange <s.e.d.lange@tue.nl>
2026-08-25 13:35:51 +0200

    Replace xx-document.md by document.md

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,7 @@ DocMeta.setdocmeta!(ProbabilisticPowerFlow, :DocTestSetup, :(using Probabilistic
 # Add titles of sections and overrides page titles
 const titles = Dict(
     # "10-tutorials" => "Tutorials", # example folder title
-    "91-developer.md" => "Developer docs",
+    "developer.md" => "Developer docs",
 )
 
 function recursively_list_pages(folder; path_prefix="")
